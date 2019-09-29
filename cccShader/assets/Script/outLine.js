a72ec2fecaf209e642f7e80f7419734b500bb8f6
“onepieces <666@66.com>
2019-09-29 23:18:43 +0800

    5555

---
+++ cccShader/assets/Script/outLine.js
@@ -0,0 +1,34 @@
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+   
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        if(this.node.getComponent(cc.Sprite)){
+            this.com = this.node.getComponent(cc.Sprite)
+        }else if(this.node.getComponent(cc.Label)){
+            this.com = this.node.getComponent(cc.Label)
+        }
+        this.material = this.com.getMaterial(0)
+        
+    },
+    start () {
+    
+        // let textureSize    = new Float32Array(2)
+        // textureSize[0]     =  this.node.getContentSize().width
+        // textureSize[1]     =  this.node.getContentSize().height
+        
+
+        let textureSize    = new Float32Array(2)
+        textureSize[0]     =  200
+        textureSize[1]     =  200
+        this.material.setProperty("u_textureSize",textureSize)
+    },
+
+   
+});
