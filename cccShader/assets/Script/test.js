39c6f853f56c6e6fe5b5a9b3bba76d470c8f7028
hukai <305258029@qq.com>
2019-10-15 17:36:15 +0800

    doy

--- cccShader/assets/Script/test.js
+++ cccShader/assets/Script/test.js
@@ -1,4 +1,4 @@
-//扭曲
+//抖音
 cc.Class({
     extends: cc.Component,
     editor: {
@@ -6,41 +6,31 @@ cc.Class({
     },
 
     properties: {
-        distortion:{
-            default:0,
-            type:cc.Float,
-            range:[0,10,0.01],
-            slide:true,
-        },
-        distortion2:{
-            default:0,
-            type:cc.Float,
-            range:[0,10,0.01],
-            slide:true,
-        },
-        speed:{
-            default:0,
-            type:cc.Float,           
-        },
-       
-
-        rollspeed:{
-            default:0,
-            type:cc.Float,           
-        },
+  
     },
     onLoad() {
         this.material = this.node.getComponent(cc.Sprite).getMaterial(0)
         this.time = 0
-        this.material.setProperty("u_distortion",this.distortion)
-        this.material.setProperty("u_distortion2",this.distortion2)
-        this.material.setProperty("u_speed",this.speed)
-
-        this.material.setProperty("u_rollspeed",this.rollspeed)
+        
+        this.flag = false
+        this.schedule(()=>{
+            if(!this.flag){
+                if(this.time>0.001){
+                    this.flag = true
+                }
+                this.time+=0.0001;
+            }else{
+                if(this.time<0){
+                    this.flag = false
+                }
+                this.time-=0.0001;
+            }
+            this.material.setProperty("scale",this.time)
+            
+        },0.005)
     },
    
     update(dt){
-        this.time+=dt;
-        this.material.setProperty("u_time",this.time)
+        
     }
 });
