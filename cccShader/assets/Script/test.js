73166322b7136627a5c9deaa4ea11eeb5a6644f5
hukai <305258029@qq.com>
2019-10-23 17:42:45 +0800

    test2

--- cccShader/assets/Script/test.js
+++ cccShader/assets/Script/test.js
@@ -6,16 +6,21 @@ cc.Class({
     },
 
     properties: {
-  
+        camera:cc.Camera
     },
     onLoad() {
         this.material = this.node.getComponent(cc.Sprite).getMaterial(0)
         this.node.on(cc.Node.EventType.TOUCH_MOVE, function (event) {
             let pos = this.node.convertToNodeSpaceAR( event.getLocation() );
-            console.log('TOUCH_MOVE',cc.v2((pos.x+320)/640,(pos.y+320)/640));
-            this.material.setProperty("u_touch", cc.v2((pos.x+320)/640,(pos.y+320)/640))
-          }, this);
-   
+            this.material.setProperty("u_touch", cc.v2((pos.x+320)/640,(-pos.y+320)/640))
+        }, this);
+
+        this.renderTexture =  new cc.RenderTexture()
+        let gl = cc.game._renderContext;
+        let width = 640;
+        let height = 640;
+        this.renderTexture.initWithSize(width, height,gl.STENCIL_INDEX8);
+        this.camera.targetTexture = this.renderTexture;
     },
    
     update(dt){
