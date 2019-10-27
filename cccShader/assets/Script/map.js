7de33f5707791f45bbe4a27a903cc5ed6878b245
“onepieces <666@66.com>
2019-10-28 00:29:58 +0800

    bug

--- cccShader/assets/Script/map.js
+++ cccShader/assets/Script/map.js
@@ -1,4 +1,4 @@
-//抖音
+
 cc.Class({
     extends: cc.Component,
     editor: {
@@ -12,12 +12,8 @@ cc.Class({
         this.material = this.node.getComponent(cc.Sprite).getMaterial(0)
         this.node.on(cc.Node.EventType.TOUCH_MOVE, function (event) {
             let pos = this.node.convertToNodeSpaceAR( event.getLocation() );
-            this.material.setProperty("u_touch", cc.v2((pos.x+480)/960,(-pos.y+320)/640))
+            this.material.setProperty("u_touch", cc.v2((pos.x+320)/640,(-pos.y+320)/640))
         }, this);
 
     },
-   
-    update(dt){
-        
-    }
 });
